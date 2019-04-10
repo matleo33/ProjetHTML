1208f545b5101978ff837c58b032c8c9f3ba7027
unknown <amaury2485@gmail.com>
2019-04-10 20:47:21 +0200

    Avancement

---
+++ src/js/validation.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    let taSoeur = document.getElementById("isbn");
+
+    console.log(taSoeur);
+
+    if (taSoeur !== null) {
+        console.log("DEFINED")
+    } else {
+        console.log("UNDEFINED")
+    }
+
+    taSoeur.onchange = function () {
+        let isbn = document.getElementById("isbn");
+
+        isbn.addEventListener("keyup", function (event) {
+            if (isbn.validity.typeMismatch) {
+                isbn.setCustomValidity("I expect an e-mail, darling!");
+            } else {
+                isbn.setCustomValidity("BLC");
+            }
+        });
+    };
+
+});
