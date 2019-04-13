dc9571985fd0d766f29541616ff2317ace04da82
unknown <amaury2485@gmail.com>
2019-04-13 16:30:24 +0200

    Ajout gestion formulaire + merge

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,38 +1,31 @@
-/*document.addEventListener("DOMContentLoaded", function(event) {
+let oeuvres = [];
 
-    let taSoeur = document.getElementById("isbn");
+window.onload = function() {
+    let myForm = document.getElementById('formulaire');
 
-    console.log(taSoeur);
+// Add a listener to the submit event
+    myForm.addEventListener('submit', function (e) {
+        let errors = [];
 
-    if (taSoeur !== null) {
-        console.log("DEFINED")
-    } else {
-        console.log("UNDEFINED")
-    }
+        // Check inputs...
 
-    taSoeur.onchange = function () {
-        let isbn = document.getElementById("isbn");
+        if(errors.length === 0) {
+            e.preventDefault(); // The browser will not make the HTTP POST request
+            let reference = document.getElementById("reference").value;
+            let titre = document.getElementById("titre").value;
+            let auteurs = document.getElementById("auteurs").value;
+            let editeurs = document.getElementById("editeurs").value;
+            let edition = document.getElementById("edition").value;
+            let annneEdition = document.getElementById("anneeEdition").value;
+            let isbn = document.getElementById("isbn").value;
+            let nbExemplaires = document.getElementById("nbExemplaires").value;
+            let disponibilite = document.getElementById("disponibilite").value;
+            let excluPret = document.getElementById("excluPret").value;
+            let commentaire = document.getElementById("commentaires").value;
 
-        isbn.addEventListener("keyup", function (event) {
-            if (isbn.validity.typeMismatch) {
-                isbn.setCustomValidity("I expect an e-mail, darling!");
-            } else {
-                isbn.setCustomValidity("BLC");
-            }
-        });
-    };
-
-});*/
-
-document.getElementById("isbn").onChange = function () {
-    console.log("in");
-    let isbn = document.getElementById("isbn");
-
-    isbn.addEventListener("", function (event) {
-        if (isbn.validity.typeMismatch) {
-            isbn.setCustomValidity("I expect an e-mail, darling!");
-        } else {
-            isbn.setCustomValidity("BLC");
+            let oeuvre = {Reference:reference, Titre:titre, Auteur:auteurs, Editeurs:editeurs, Edition:edition, AnnneEdition:annneEdition,
+                ISBN:isbn, NbExemplaires:nbExemplaires, Disponibilite:disponibilite, ExcluPret:excluPret, Commentaire:commentaire};
+            oeuvres.push(oeuvre);
         }
     });
 };
